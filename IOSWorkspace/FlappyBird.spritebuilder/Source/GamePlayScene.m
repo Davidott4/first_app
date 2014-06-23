a4b5b76d57f37cdeba6966a19347442e996471ca
dott <dott@mail.ccsf.edu>
2014-06-23 13:29:36 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ IOSWorkspace/FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -0,0 +1,37 @@
+#import "GamePlayScene.h"
+#import "Character.h"
+#import "Obstacle.h"
+
+@implementation GamePlayScene
+
+- (void)initialize
+{
+    // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+}
+
+-(void)update:(CCTime)delta
+{
+    // put update code here
+}
+
+// put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
+
+-(void) doSomething
+{
+    NSLog(@"Hello World!");
+}
+
+-(int) doSomethingWithAString: (NSString*) myString
+{
+    //returns the length of the string times ten
+    return [myString length] * 10;
+}
+
+
+@end
